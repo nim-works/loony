10125344d510c268c82dd5afb27309726ac00434
Shayan Habibi <shayanftw@gmail.com>
2021-09-26 16:26:35 +0800

    Added a simple coherance test

---
+++ tests/testbasiccoherance.nim
@@ -0,0 +1,137 @@
+import std/osproc
+import std/strutils
+import std/logging
+import std/atomics
+import std/os
+import std/macros
+
+import balls
+import cps
+
+import loony
+
+const
+  continuationCount = when defined(windows): 1_000 else: 10_000
+let
+  threadCount = when defined(danger): countProcessors() else: 1
+
+type
+  C = ref object of Continuation
+    a: string
+
+addHandler newConsoleLogger()
+setLogFilter:
+  when defined(danger):
+    lvlNotice
+  elif defined(release):
+    lvlInfo
+  else:
+    lvlDebug
+
+var q: LoonyQueue[Continuation]
+
+proc runThings() {.thread.} =
+  while true:
+    var job = pop q
+    if job.dismissed:
+      break
+    else:
+      while job.running:
+        job = trampoline job
+
+proc enqueue(c: C): C {.cpsMagic.} =
+  check not q.isNil
+  c.a.add 'a'
+  q.push(c)
+
+var counter {.global.}: Atomic[int]
+
+# try to delay a reasonable amount of time despite platform
+when defined(windows):
+  proc noop(c: C): C {.cpsMagic.} =
+    sleep:
+      when defined(danger):
+        1
+      else:
+        0 # 🤔
+    c
+else:
+  import posix
+  proc noop(c: C): C {.cpsMagic.} =
+    if c.a != "aa":
+      echo c.a
+      raise
+    else:
+      c.a = "banana"
+    const
+      ns = when defined(danger): 1_000 else: 10_000
+    var x = Timespec(tv_sec: 0.Time, tv_nsec: ns)
+    var y: Timespec
+    if 0 != nanosleep(x, y):
+      raise
+    c
+
+proc assertB(c: C): C {.cpsMagic.} =
+  if c.a != "bananaa":
+    raise  
+  c
+
+proc doContinualThings() {.cps: C.} =
+  enqueue()
+  noop()
+  enqueue()
+  assertB()
+  discard counter.fetchAdd(1)
+
+template expectCounter(n: int): untyped =
+  ## convenience
+  try:
+    check counter.load == n
+  except Exception:
+    checkpoint " counter: ", load counter
+    checkpoint "expected: ", n
+    raise
+
+suite "loony":
+  block:
+    ## creation and initialization of the queue
+
+    # Moment of truth
+    q = initLoonyQueue[Continuation]()
+
+  block:
+    ## run some continuations through the queue in another thread
+    when defined(danger): skip "boring"
+    var thr: Thread[void]
+
+    counter.store 0
+    dumpAllocStats:
+      for i in 0 ..< continuationCount:
+        var c = whelp doContinualThings()
+        discard enqueue c
+      createThread(thr, runThings)
+      joinThread thr
+      expectCounter continuationCount
+
+  block:
+    ## run some continuations through the queue in many threads
+    when not defined(danger): skip "slow"
+    var threads: seq[Thread[void]]
+    newSeq(threads, threadCount)
+
+    counter.store 0
+    dumpAllocStats:
+      for i in 0 ..< continuationCount:
+        var c = whelp doContinualThings()
+        discard enqueue c
+      checkpoint "queued $# continuations" % [ $continuationCount ]
+
+      for thread in threads.mitems:
+        createThread(thread, runThings)
+      checkpoint "created $# threads" % [ $threadCount ]
+
+      for thread in threads.mitems:
+        joinThread thread
+      checkpoint "joined $# threads" % [ $threadCount ]
+
+      expectCounter continuationCount
