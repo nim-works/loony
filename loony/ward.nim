491bfb66a8a5673bad2ad5d51c0c35fadb860b8a
shayanhabibi <shayanftw@gmail.com>
2021-10-02 14:07:11 +0800

    style

--- loony/ward.nim
+++ loony/ward.nim
@@ -112,18 +112,22 @@ proc unsafePop*[T, F](ward: Ward[T, F]): T =
 
 proc pause*[T, F](ward: Ward[T, F]): bool =
   when ward.flags.contains Pausable:
-    if `and`(ward.values.fetchOr({PopPausable, PushPausable}, moRelease), {PopPausable, PushPausable}) > 0'u16:
+    if `and`(ward.values.fetchOr({PopPausable, PushPausable}, moRelease),
+            {PopPausable, PushPausable}) > 0'u16:
       # The ward has now been paused
       result = true
     else:
       # The ward was already paused
       result = false
   elif ward.flags.contains PushPausable:
-    raise newException(ValueError, "Have to use pausePush unless the Pausable flag is used")
+    raise newException(ValueError,
+                      "Have to use pausePush unless the Pausable flag is used")
   elif ward.flags.contains PopPausable:
-    raise newException(ValueError, "Have to use pausePop unless the Pausable flag is used")
+    raise newException(ValueError,
+                      "Have to use pausePop unless the Pausable flag is used")
   else:
-    raise newException(ValueError, "Ward requires the pausable flag")
+    raise newException(ValueError,
+                      "Ward requires the pausable flag")
 
 proc pausePush*[T, F](ward: Ward[T, F]): bool =
   when ward.flags.contains PushPausable:
@@ -134,7 +138,8 @@ proc pausePush*[T, F](ward: Ward[T, F]): bool =
       # The pushes were already paused
       result = false
   else:
-    raise newException(ValueError, "Ward requires the pushpausable or pausable flag")
+    raise newException(ValueError,
+                      "Ward requires the pushpausable or pausable flag")
 
 proc pausePop*[T, F](ward: Ward[T, F]): bool =
   when ward.flags.contains PopPausable:
@@ -145,41 +150,50 @@ proc pausePop*[T, F](ward: Ward[T, F]): bool =
       # The pops were already paused
       result = false
   else:
-    raise newException(ValueError, "Ward requires the poppausable or pausable flag")
+    raise newException(ValueError,
+                      "Ward requires the poppausable or pausable flag")
 
 proc resume*[T, F](ward: Ward[T, F]): bool =
   when ward.flags.contains Pausable:
-    if `and`(ward.values.fetchAnd(complement({PushPausable, PopPausable}), moRelease), {PushPausable, PopPausable}) > 0'u16:
+    if `and`(ward.values.fetchAnd(complement({PushPausable, PopPausable}), moRelease),
+            {PushPausable, PopPausable}) > 0'u16:
       # The ward is now resumed
       result = true
     else:
       # The ward was not paused
       result = false
   elif ward.flags.contains PushPausable:
-    raise newException(ValueError, "Have to use resumePush unless the Pausable flag is used")
+    raise newException(ValueError,
+                      "Have to use resumePush unless the Pausable flag is used")
   elif ward.flags.contains PopPausable:
-    raise newException(ValueError, "Have to use resumePop unless the Pausable flag is used")
+    raise newException(ValueError,
+                      "Have to use resumePop unless the Pausable flag is used")
   else:
-    raise newException(ValueError, "Ward requires the pausable flag")
+    raise newException(ValueError,
+                      "Ward requires the pausable flag")
 
 proc resumePush*[T, F](ward: Ward[T, F]): bool =
   when ward.flags.contains PushPausable:
-    if `and`(ward.values.fetchAnd(complement({PushPausable}), moRelease), {PushPausable}) > 0'u16:
+    if `and`(ward.values.fetchAnd(complement({PushPausable}), moRelease),
+            {PushPausable}) > 0'u16:
       # The ward is now resumed
       result = true
     else:
       # The ward was not paused
       result = false
   else:
-    raise newException(ValueError, "Ward requires the pausable or pushpausable flag")
+    raise newException(ValueError,
+                      "Ward requires the pausable or pushpausable flag")
 
 proc resumePop*[T, F](ward: Ward[T, F]): bool =
   when ward.flags.contains PopPausable:
-    if `and`(ward.values.fetchAnd(complement({PopPausable}), moRelease), {PopPausable}) > 0'u16:
+    if `and`(ward.values.fetchAnd(complement({PopPausable}), moRelease),
+            {PopPausable}) > 0'u16:
       # The ward is now resumed
       result = true
     else:
       # The ward was not paused
       result = false
   else:
-    raise newException(ValueError, "Ward requires the pausable or poppausable flag")
+    raise newException(ValueError,
+                      "Ward requires the pausable or poppausable flag")
