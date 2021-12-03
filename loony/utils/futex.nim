830954e2c9a81927422fd25da37e171698e70a2c
Shayan Habibi <shayanftw@gmail.com>
2021-12-03 13:00:35 +0800

    [MacOS/Darwin] Emulating Futex behaviour Fixes #21

--- loony/utils/futex.nim
+++ loony/utils/futex.nim
@@ -1,6 +1,16 @@
 when defined(windows):
   import ./futex/futex_windows
   export futex_windows
-else:
+elif defined(linux):
   import ./futex/futex_linux
   export futex_linux
+elif defined(macosx):
+  import ./futex/futex_darwin
+  export futex_darwin
+else:
+  proc wait*[T](monitor: ptr T; compare: T) {.inline.} =
+    {.fatal: "Your OS is not supported with implemented futexes, please submit an issue".}
+  proc wake*(monitor: pointer) {.inline.} =
+    {.fatal: "Your OS is not supported with implemented futexes, please submit an issue".}
+  proc wakeAll*(monitor: pointer) {.inline.} =
+    {.fatal: "Your OS is not supported with implemented futexes, please submit an issue".}
